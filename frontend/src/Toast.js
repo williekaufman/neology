e0fe038544adbec4415187c12c4b1181b8030ac1
Willie <williekaufman1@gmail.com>
2024-05-01 02:24:45 -0400

    x

--- frontend/src/Toast.js
+++ frontend/src/Toast.js
@@ -7,7 +7,7 @@ const Toast = ({ message, onClose, style, isError=true }) => {
     <div className={isError ? "error-toast" : "happy-toast"} style={style}>
       <div className="error-toast-content">
         <Typography className="error-toast-message">{message}</Typography>
-        <Button className="error-toast-close" onClick={onClose}>
+        <Button className="error-toast-close no-hover" onClick={onClose}>
           &times;
         </Button>
       </div>
